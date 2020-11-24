fc513d9e721aa7513a8763237d9133eca45bb90b
kruchkamgar <kruchkamgar@gmail.com>
2020-11-24 12:42:01 -0700

    basic search function, incl form

--- src/App.js
+++ src/App.js
@@ -9,21 +9,25 @@ function App() {
   const headerNames = [
     'name', 'city', 'state', 'genre', 'attire', 'phoneNumber'
   ]
-  const [rowData, setRowData] = useState([
-    {name: "Capri"},
+  const [aPIData, setaPIData] = useState([
+    {name: "Capri", city: "Denver"},
     {name: "Pho4U"}
-  ]);
+  ])
+  const [rowData, setRowData] = useState(aPIData);
   const [column, setColumn] = useState();
 
   useEffect(() => {
     sortByColumn(rowData, column)
   }, [column])
+  useEffect(() => {
+    searchDomain(rowData);
+  }, [rowData])
 
   const headers =
   headerNames.map( name => {
     return <Header key={name} name={name}
-        clickEvent={(element) => setColumn(
-          element.target.getAttribute('name')) }
+        clickEvent={(event) => setColumn(
+          event.target.getAttribute('name')) }
       >name</Header>
   });
 
@@ -39,8 +43,36 @@ function App() {
     });
   }
 
+  const searchCols = ['name', 'city', 'genre'];
+
+  // memoize this domain
+  const searchDomain = (data) => {
+    return data.map( (datum, index) => {
+      return { index: index,
+        values: searchCols.map( col => datum[col] ) }
+    });
+  }
+
+  const search = (data, searchCols, searchField) => {
+    const domain = searchDomain(data);
+    const matchedIndices = [];
+    domain.forEach( (datum, index) => {
+      if (datum.values.find(value => value === searchField)) {
+        matchedIndices.push(datum.index) }
+    });
+    setRowData(
+      matchedIndices.map(index => data[index]) )
+  }
+
   return (
     <div className="App">
+      <input
+        type="text"
+        placeholder="name, city, or genre"
+        onKeyUp={ (event) => {
+          if (event.keyCode === 13) {
+            search(rowData, searchCols, event.target.value) } }}
+      />
       <Table headers={headers} rows={rows}></Table>
     </div>
   );
