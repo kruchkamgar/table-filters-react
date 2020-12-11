7ea8fe776175c2cf5f8b5cb0cc25d007ee4c0d39
kruchkamgar <kruchkamgar@gmail.com>
2020-12-10 20:07:47 -0700

    add case sensitivity for 'state' category

--- src/config/headersConfig.js
+++ src/config/headersConfig.js
@@ -1,3 +1,3 @@
-const headersConfig = {'name': false, 'city':false,'state':false, 'telephone':false, 'genre': {delimiter: ','}, 'attire': {caseSensitivity: false, exactTerms: true} };
+const headersConfig = {'name': false, 'city':false,'state': {caseSensitivity: true}, 'telephone':false, 'genre': {delimiter: ','}, 'attire': {caseSensitivity: false, exactTerms: true} };
 
 export default headersConfig;
