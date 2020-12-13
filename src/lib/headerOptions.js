19e086664baf8813a739c636f14498adff9d8089
kruchkamgar <kruchkamgar@gmail.com>
2020-12-13 16:38:44 -0700

    adapt function to also set baseline filterResults

--- src/lib/headerOptions.js
+++ src/lib/headerOptions.js
@@ -1,16 +1,13 @@
-const getHeaderOptions = (data, headersConfig) => {
+const getHeaderOptions = (data, headersConfig, setPriors=false) => {
   const headerOptions = {};
-  for( const [name, config] of Object.entries(headersConfig) ){
-    const queuedOptions = [];
-    for (const datum in data) {
-      if(config.delimiter) {
-        const split = data[datum][name].split(config.delimiter);
-        queuedOptions.push(...split); }
-      else { queuedOptions.push(data[datum][name]); } };
-    if(!config.caseSensitivity) {
-      for(let option in queuedOptions){
-        queuedOptions[option] = queuedOptions[option].toLowerCase(); }}
-
+  for( const [column, config] of Object.entries(headersConfig) ){
+    let queuedOptions = []
+    if(setPriors){
+      if(data[column] === undefined) continue;
+      queuedOptions = formOptions(data[column], column, config) }
+    else{
+      queuedOptions = formOptions(data, column, headersConfig);
+    }
     //feature: use 'filterResult' to calculate the number of rows matching each filter option (queuedOptions)
       // calculate it once for, and independently of, each filterBy entry
     // const options = new Set(['(all)']);
@@ -18,8 +15,21 @@ const getHeaderOptions = (data, headersConfig) => {
     for(const option of queuedOptions) {
       options[option] = (options[option] || 0) + 1; }
       // options.add(option); }
-    headerOptions[name] = options; }
+    headerOptions[column] = options; }
   return headerOptions;
+}
+
+const formOptions = (data, column, config) => {
+  const queuedOptions = [];
+  for (const datum in data) {
+    if(config.delimiter) {
+      const split = data[datum][column].split(config.delimiter);
+      queuedOptions.push(...split); }
+    else { queuedOptions.push(data[datum][column]); } };
+  if(!config.caseSensitivity) {
+    for(let option in queuedOptions){
+      queuedOptions[option] = queuedOptions[option].toLowerCase(); }}
+  return queuedOptions;
 }
 
 export default getHeaderOptions
